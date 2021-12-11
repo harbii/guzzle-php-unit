931ef68d21817d3931d0d6a4b6e4fd74e8de0714
harbii <mahmoud.samy@black-squares.com>
2021-12-11 19:28:57 +0200

    start

---
+++ README.md
@@ -0,0 +1,5 @@
+# tools to test any server 
+
+set new end point path in phpunit.xml
+
+change BASE_URI to new url server domin
--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -2,10 +2,26 @@
 
 namespace Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase {
 
 	use HttpRequests ;
+
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = BASE_URI ;
+
+    /**
+     * Creates the application.
+     *
+     * @return \Laravel\Lumen\Application
+     */
+    public function createApplication( ) {
+        return require __DIR__ . '/../App/Bootstrap/App.php' ;
+    }
 
 }
